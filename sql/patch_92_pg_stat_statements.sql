c210d0f88b9b3b5c6c2e6576047b1123cf949c10
Oleg <oleg.ispu@yandex.ru>
2017-05-08 23:01:28 +0300

    added pg_stat_statements patch for 9.2 vesion of PostgreSQL

---
+++ sql/patch_92_pg_stat_statements.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION pg_stat_statements(boolean)
+  RETURNS table (userid oid,
+dbid oid,
+query text,
+calls bigint,
+total_time double precision,
+rows bigint,
+shared_blks_hit bigint,
+shared_blks_read bigint,
+shared_blks_dirtied bigint,
+shared_blks_written bigint,
+local_blks_hit bigint,
+local_blks_read bigint,
+local_blks_dirtied bigint,
+local_blks_written bigint,
+temp_blks_read bigint,
+temp_blks_written bigint,
+blk_read_time double precision,
+blk_write_time double precision) AS
+  '
+ select userid,
+    dbid,
+    query,
+    calls,
+    total_time,
+    rows,
+    shared_blks_hit,
+    shared_blks_read,
+    shared_blks_dirtied,
+    shared_blks_written,
+    local_blks_hit,
+    local_blks_read,
+    local_blks_dirtied,
+    local_blks_written,
+    temp_blks_read,
+    temp_blks_written,
+    blk_read_time,
+    blk_write_time from pg_stat_statements()
+ '
+  LANGUAGE 'sql' VOLATILE
+  COST 1
+  ROWS 1000;
