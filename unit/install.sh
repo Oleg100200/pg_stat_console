fbfc00b2428c06e946c535303553e3e51398396a
Oleg <oleg.ispu@yandex.ru>
2017-10-13 00:16:18 +0300

    main node installer, part 4

--- unit/install.sh
+++ unit/install.sh
@@ -4,11 +4,19 @@ PYTHON='/usr/local/bin/python3.6'
 PSC_PATH_INSTALL=$PWD
 PSC_PATH="$(dirname "$PSC_PATH_INSTALL")"
 
+cd unit
 for f in pg_stat_*; do if [[ -f "$f" ]]; then
-	sed -i "s|PYTHON|$PYTHON|g" $f
-	sed -i "s|PSC_PATH|$PSC_PATH|g" $f
-	cp $PSC_PATH_INSTALL/$f /usr/lib/systemd/system
-	chmod 664 /usr/lib/systemd/system/$f
+	if [[ $f == *".service"* ]]; then
+		echo "Processing " $f
+		sed -i "s|PYTHON|$PYTHON|g" $f
+		sed -i "s|PSC_PATH|$PSC_PATH|g" $f
+		cp $PSC_PATH_INSTALL/unit/$f /usr/lib/systemd/system
+		chmod 664 /usr/lib/systemd/system/$f
+	else
+		echo -e "\nWrong input file " $f
+	fi
 fi; done
 
 systemctl daemon-reload
+
+echo -e "\nsystemctl has been reloaded"
